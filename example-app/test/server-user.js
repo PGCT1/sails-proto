79e5f973dad9eb51e0b961f5911f0fd353643be2
PGCT1 <t1gemini@yandex.ru>
2014-06-07 02:19:45 +0400

    further test organization

--- example-app/test/server-user.js
+++ example-app/test/server-user.js
@@ -31,75 +31,83 @@ describe('User API Tests', function(){
 			User = app.sails.models.user;
 		});
 
-		it('should find 0 users for empty database',function(done){
+		describe('Prototype',function(){
 
-			User.find({},function(e,users){
-				users.should.have.a.lengthOf(0);
-				done();
-			});
+			it('should find 0 users for empty database',function(done){
 
-		});
-
-		it('should fail create for invalid username',function(done){
-
-				User.create(
-					{
-						'name':UserTemplate.invalid.name,
-						'password':UserTemplate.valid.password
-					},function(err){
-						(err != null).should.be.true;
-						done();
+				User.find({},function(e,users){
+					users.should.have.a.lengthOf(0);
+					done();
 				});
 
-		});
+			});
 
-		it('should fail create for invalid password',function(done){
+			it('should fail create for invalid username',function(done){
 
-				User.create(
-					{
-						'name':UserTemplate.valid.name,
-						'password':UserTemplate.invalid.password
-					},function(err){
-						(err != null).should.be.true;
-						done();
+					User.create(
+						{
+							'name':UserTemplate.invalid.name,
+							'password':UserTemplate.valid.password
+						},function(err){
+							(err != null).should.be.true;
+							done();
+					});
+
+			});
+
+			it('should fail create for invalid password',function(done){
+
+					User.create(
+						{
+							'name':UserTemplate.valid.name,
+							'password':UserTemplate.invalid.password
+						},function(err){
+							(err != null).should.be.true;
+							done();
+					});
+
+			});
+
+			it('should create successfully for valid user',function(done){
+
+				var newTemplate = JSON.parse(JSON.stringify(UserTemplate));
+
+				User.create(newTemplate.valid,function(err){
+					(err === null).should.be.true;
+					done();
 				});
 
-		});
+			});
 
-		it('should create successfully for valid user',function(done){
+			it('should find 1 user',function(done){
 
-			var newTemplate = JSON.parse(JSON.stringify(UserTemplate));
+				User.find({},function(e,users){
+					users.should.have.a.lengthOf(1);
+					done();
+				});
 
-			User.create(newTemplate.valid,function(err){
-				(err === null).should.be.true;
-				done();
+			});
+
+			it('should not store passwords',function(done){
+
+				User.findOne({"name":UserTemplate.valid.name},function(e,user){
+					user.should.not.have.a.property('password');
+					done();
+				});
+
 			});
 
 		});
 
-		it('should find 1 user',function(done){
+		describe('Instance with extensions',function(){
 
-			User.find({},function(e,users){
-				users.should.have.a.lengthOf(1);
-				done();
+			it('should say return true if .isCool is called on User',function(done){
+
+				if(User.isCool()){
+					done();
+				}
+
 			});
-
-		});
-
-		it('should not store passwords',function(done){
-
-			User.findOne({"name":UserTemplate.valid.name},function(e,user){
-				user.should.not.have.a.property('password');
-				done();
-			});
-
-		});
-
-		it('should say return true if .isCool is called on User',function(done){
-
-			if(User.isCool()){
-				done();
-			}
 
 		});
 
@@ -111,36 +119,42 @@ describe('User API Tests', function(){
 			UserController = app.sails.controllers.user;
 		});
 
-		it('should successfully login with valid credentials',function(done){
+		describe('Prototype',function(){
 
-			UserController.login(
-				new SailsMock.PostRequest(UserTemplate.valid),
-				new SailsMock.Ok(done)
-			);
+			it('should successfully login with valid credentials',function(done){
+
+				UserController.login(
+					new SailsMock.PostRequest(UserTemplate.valid),
+					new SailsMock.Ok(done)
+				);
+
+			});
+
+			it('should not successfully login with invalid credentials',function(done){
+
+				UserController.login(
+					new SailsMock.PostRequest({
+						'name':UserTemplate.valid.name,
+						'password':UserTemplate.invalid.password
+					}),
+					new SailsMock.Unauthorized(done)
+				);
+
+			});
 
 		});
 
-		it('should not successfully login with invalid credentials',function(done){
+		describe('Instance with extensions',function(){
+			
+			it('should say send "awesome" if .awesome is called on controller',function(done){
 
-			UserController.login(
-				new SailsMock.PostRequest({
-					'name':UserTemplate.valid.name,
-					'password':UserTemplate.invalid.password
-				}),
-				new SailsMock.Unauthorized(done)
-			);
+				UserController.awesome(null,{send:function(msg){
+					if(msg == 'awesome'){
+						done();
+					}
+				}});
 
-		});
-
-		// test prototype extensions
-
-		it('should say send "awesome" if .awesome is called on controller',function(done){
-
-			UserController.awesome(null,{send:function(msg){
-				if(msg == 'awesome'){
-					done();
-				}
-			}});
+			});
 
 		});
 
