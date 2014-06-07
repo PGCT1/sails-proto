beb7c1eb55daa365cc7adef8e8e86439155d76b3
PGCT1 <t1gemini@yandex.ru>
2014-06-07 04:38:55 +0400

    normalized include syntax

--- sails-proto.js
+++ sails-proto.js
@@ -1,8 +1,4 @@
 'use strict';
 
-module.exports.user = {
-	controller:require('./prototypes/user/controller.js'),
-	model:require('./prototypes/user/model.js')
-};
-
+module.exports.user = require('./prototypes/user/index.js');
 module.exports.response = require('./prototypes/response/index.js');
