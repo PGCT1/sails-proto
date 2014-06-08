fc0c3a1f89e0b557a3518b8a273d8acf6fb43964
PGCT1 <t1gemini@yandex.ru>
2014-06-08 22:05:01 +0400

    fixed typo in user controller prototype

--- prototypes/user/controller.js
+++ prototypes/user/controller.js
@@ -24,7 +24,7 @@ var controller = function(){
 
 	this.logout = function(req,res){
 		delete req.session.userId;
-		this.respond.bind({'req':req,'res':res}).ok();
+		this.respond.ok.bind({'req':req,'res':res})();
 	};
 
 };
