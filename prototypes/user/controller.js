16bab1f427cee2d63960671fa918a8e87241ee30
PGCT1 <t1gemini@yandex.ru>
2014-06-10 23:49:31 +0400

    added accesscontrol for user update

--- prototypes/user/controller.js
+++ prototypes/user/controller.js
@@ -2,6 +2,22 @@
 
 var rootController = require('../root/controller.js');
 
+var idMatch = function(objectName,req,res,next){
+
+	var id = req.params.id;
+
+	if(!id){
+		return sails.controllers[objectName].respond.badRequest.bind({'req':req,'res':res})('Missing id parameter.');
+	}
+
+	if(id == req.session[objectName+'Id']){
+		next();
+	}else{
+		sails.controllers[objectName].respond.unauthorized.bind({'req':req,'res':res})();
+	}
+
+}
+
 var controller = function(config){
 
 	// default name for the model is 'user', but any name can be used
@@ -35,26 +51,45 @@ var controller = function(config){
 		sails.controllers[objectName].respond.ok.bind({'req':req,'res':res})();
 	};
 
+	this.update = function(req,res){
+
+		var id = req.params.id;
+
+		idMatch(objectName,req,res,function(){
+
+			sails.models[objectName].update({id:id},req.body,function(err){
+
+				if(err){
+					sails.controllers[objectName].respond.serverError.bind({'req':req,'res':res})(err);
+				}else{
+					sails.controllers[objectName].respond.ok.bind({'req':req,'res':res})();
+				}
+
+			});
+
+		});
+
+	};
+
 	this.destroy = function(req,res){
 
 		var id = req.params.id;
 
-		if(!id){
-			return sails.controllers[objectName].respond.badRequest.bind({'req':req,'res':res})('Missing id parameter.');
-		}
+		idMatch(objectName,req,res,function(){
 
-		if(id == req.session[objectName+'Id']){
 			sails.models[objectName].destroy({id:id}).exec(function(err){
+
 				if(!err){
 					delete req.session[objectName+'Id'];
 					sails.controllers[objectName].respond.ok.bind({'req':req,'res':res})();
 				}else{
 					sails.controllers[objectName].respond.serverError.bind({'req':req,'res':res})(err);
 				}
+
 			});
-		}else{
-			sails.controllers[objectName].respond.unauthorized.bind({'req':req,'res':res})();
-		}
+
+		});
+			
 	};
 
 };
